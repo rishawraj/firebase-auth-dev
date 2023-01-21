af6a6db0bbe48aa1b39cb233622114c1cb3cdd00
rishaw <rishawraj0703@gmail.com>
2023-01-21 15:03:55 +0530

    add login signup routes

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import Signup from "./Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+        {/* <Signup /> */}
+      </div>
+    </Container>
+  );
+}
+
+export default App;
